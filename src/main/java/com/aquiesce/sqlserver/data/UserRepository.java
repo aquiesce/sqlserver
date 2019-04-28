d7b2bb7f5d21d37c972248570c207bd84b2c450a
Matt Blewitt <matt@aquiesce.com>
2019-04-28 16:19:44 +0100

    JpaRepo

--- src/main/java/com/aquiesce/sqlserver/data/UserRepository.java
+++ src/main/java/com/aquiesce/sqlserver/data/UserRepository.java
@@ -1,9 +1,10 @@
 package com.aquiesce.sqlserver.data;
 
 import com.aquiesce.sqlserver.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByFirstName(String firstName);
     User findByLastName(String lastName);
